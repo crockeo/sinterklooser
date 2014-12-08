83d37ecd68aff307589c87beb5b67777e07b721e
Cerek Hillen <crockeo@gmail.com>
2014-12-08 15:44:28 -0500

    Added a function to do some graph processing. I need to write it.

---
+++ src/processing.cpp
@@ -0,0 +1,10 @@
+#include "processing.hpp"
+
+//////////
+// Code //
+
+// Determining the list of pairs for Sinterklaas.
+std::tuple<int, int>* findPairing(Graph* g) {
+    // TODO: Actually do something
+    return nullptr;
+}
---
+++ src/processing.hpp
@@ -0,0 +1,16 @@
+#ifndef _PROCESSING_HPP_
+#define _PROCESSING_HPP_
+
+//////////////
+// Includes //
+#include <tuple>
+
+#include "graph.hpp"
+
+//////////
+// Code //
+
+// Determining the list of pairs for Sinterklaas.
+std::tuple<int, int>* findPairing(Graph*);
+
+#endif
