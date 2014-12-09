12c3361be8f267130adf8f026bcc6620ce36a5f5
Cerek Hillen <crockeo@gmail.com>
2014-12-09 01:16:30 -0500

    Fleshed out the command-line front end.

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,28 @@
 // Includes //
 #include <iostream>
 
+#include "processing.hpp"
+#include "graph.hpp"
+#include "io.hpp"
+
 //////////
 // Code //
 
-int main() {
-    std::cout << "Hello world!\n";
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        std::cerr << "Proper usage: sinterklooser <data file>\n";
+        return 1;
+    }
+
+    Graph* g = loadGraph(std::string(argv[1]));
+    
+    if (g == nullptr) {
+        std::cerr << "Could not load the file " << argv[1] << "!\n";
+        return 2;
+    }
+
+    printPairing(g, findPairing(g));
+
+    delete g;
     return 0;
 }
