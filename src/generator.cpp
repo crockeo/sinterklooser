9bcc18940310b3a63fe39dcf44378225b6ec7c9c
Cerek Hillen <crockeo@gmail.com>
2014-12-10 16:25:36 -0500

    Added the beginnings of a generator for dataset files.

---
+++ src/generator.cpp
@@ -0,0 +1,12 @@
+//////////////
+// Includes //
+#include <iostream>
+
+//////////
+// Code //
+
+// The entry point to the application.
+int main(int argc, char** argv) {
+    std::cout << "Welcome to the digraph generator!\n"
+              << "Follow the instructions below to get started!\n";
+}
