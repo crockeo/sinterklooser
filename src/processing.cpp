90e7ac19f39aadaf5113a780c094b21492554fb4
Cerek Hillen <crockeo@gmail.com>
2014-12-18 20:50:19 -0500

    Processing = rewrote.

--- src/processing.cpp
+++ src/processing.cpp
@@ -13,82 +13,53 @@
 //////////
 // Code //
 
-//using std::make_tuple;
-//using std::vector;
-//using std::tuple;
-//using std::get;
-//using std::set;
-
-//// Getting a random number with a max value.
-//int randMax(int n) { return rand() % n; }
-
-//// Creating an array of nodes that are connected to another node.
-//std::vector<int> findNodes(const Graph& g, int i) {
-    //std::vector<int> nodes;
-
-    //for (int j = 0; j < g.getSize(); j++)
-        //if (g.connected(i, j))
-            //nodes.push_back(j);
-
-    //return nodes;
-//}
-
-//vector<int> findOpen(const Graph& g, std::set<int> gotten) {
-    //vector<int> opens;
-
-    //for (int i = 0; i < g.getSize(); i++)
-        //if (gotten.count(i) == 0)
-            //opens.push_back(i);
-
-    //return opens;
-//}
-
-//// Determining the list of pairs for Sinterklaas.
-//std::vector<std::tuple<int, int>> findPairing(Graph g) {
-    //std::vector<std::tuple<int, int>> pairs;
-    //std::set<int> gotten;
-    //for (int i = 0; i < g.getSize(); i++) {
-        //std::vector<int> nodes = findNodes(g, i);
-        //int target;
-
-        //if (nodes.size() == 0) {
-            //std::vector<int> opens = findOpen(g, gotten);
-            //target = opens[randMax(opens.size())];
-        //} else
-            //target = nodes[randMax(nodes.size())];
-
-        //pairs.push_back(make_tuple(i, target));
-        //gotten.insert(target);
-
-        //g.removeEdge(target, i);
-        //for (int j = 0; j < g.getSize(); j++)
-            //g.removeEdge(j, target);
-    //}
-
-    //return pairs;
-//}
-
-//// Printing out the set of pairings.
-//void printPairing(const Graph& g, std::vector<std::tuple<int, int>> pairs) {
-    //std::cout << "Pairing list:\n";
-    //for (int i = 0; i < pairs.size(); i++)
-        //std::cout << "  " << g.getName(get<0>(pairs[i])) << " gets " << g.getName(get<1>(pairs[i])) << "!\n";
-//}
+// Generating a random number with a maximum value.
+int maxRand(int max) { return rand() % max; }
 
 // Finding all of the connections a given node on a Graph contains.
-std::vector<int> findConnections(const Graph& g, int src) {
+std::vector<int> findConnections(const Graph& g, const std::set<int>& gotten, int src) {
     std::vector<int> conns;
 
     for (int i = 0; i < g.getSize(); i++)
-        if (g.connected(src, i))
+        if (i != src && g.connected(src, i) && gotten.count(i) == 0)
             conns.push_back(i);
 
     return conns;
 }
 
+// Finding all of the nodes that haven't been gotten yet.
+std::vector<int> notGotten(const Graph& g, const std::set<int>& gotten) {
+    std::vector<int> ng;
+    for (int i = 0; i < g.getSize(); i++)
+        if (gotten.count(i) == 0)
+            ng.push_back(i);
+    return ng;
+}
+
 // Determining the list of pairs for Sinterklaas.
 std::vector<std::tuple<int, int>> findPairing(Graph g) {
     std::vector<std::tuple<int, int>> ns;
+    std::set<int> gotten;
+
+    std::vector<int> conns;
+    int target;
+
+    for (int i = 0; i < g.getSize(); i++) {
+        conns = findConnections(g, gotten, i);
+        if (conns.size() == 0)
+            conns = notGotten(g, gotten);
+
+        if (conns.size() == 0) {
+            std::cout << "WHOOPS.\n";
+            return ns;
+        }
+        
+        target = conns[maxRand(conns.size())];
+        ns.push_back(std::make_tuple(i, target));
+
+        gotten.insert(target);
+    }
+
     return ns;
 }
 
